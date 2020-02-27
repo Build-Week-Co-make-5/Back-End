83b20e710147ced5b4e1c4ed9d5d2e11a8001e26
Juan Aleman <juan.j.aleman@gmail.com>
2020-02-26 22:25:01 -0600

    fixed .env file issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT;
-server.listen(port, () => {
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
