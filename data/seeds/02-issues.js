998067020b5c5ae8e5ec8a84a7b3bf6c07c9134f
Juan Aleman <juan.j.aleman@gmail.com>
2020-03-05 19:17:18 -0600

    deleted img folder and added img url to seed data

--- data/seeds/02-issues.js
+++ data/seeds/02-issues.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       issue_location: "8207 High Ridge Circle",
       category: "roads",
       priority: "high",
-      imgURL: "../img/pothole.jpg",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457226/pothole-min_wmcc9j.jpg",
       issue_details:
         "I asked Domino's to come fix it but they said they're not doing that in our state yet."
     },
@@ -15,7 +15,7 @@ exports.seed = function(knex) {
       issue_location: "7723 South Lane",
       category: "debris",
       priority: "low",
-      imgURL: "https://unsplash.com/photos/ATsfZ_x8lZg",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457221/debris-min_qhcjf5.jpg",
       issue_details: "There are sticks and leaves all over the sidewalk here"
     },
     {
@@ -23,7 +23,7 @@ exports.seed = function(knex) {
       issue_location: "Park on Myrtle Ave",
       category: "landscape",
       priority: "low",
-      imgURL: "https://unsplash.com/photos/h2i3A8SuKkw",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457228/overgrown_grass-min_fgcqry.jpg",
       issue_details:
         "The grass at this playground is getting out of control! Please schedule it to be mowed ASAP"
     },
@@ -32,7 +32,7 @@ exports.seed = function(knex) {
       issue_location: "2977 N 1412 E",
       category: "roads",
       priority: "medium",
-      imgURL: "",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457226/broken_curb-min_puvr1v.jpg",
       issue_details: "The curb is falling apart here and needs fixed again"
     },
     {
@@ -40,7 +40,7 @@ exports.seed = function(knex) {
       issue_location: "Needs pruning",
       category: "landscape",
       priority: "low",
-      imgURL: "",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457222/pruning-min_jjwjks.jpg",
       issue_details:
         "People walk in the road because the shrubs and trees are so overgrown here"
     },
@@ -49,7 +49,7 @@ exports.seed = function(knex) {
       issue_location: "8277 Newbridge Ave",
       category: "debris",
       priority: "low",
-      imgURL: "",
+      imgURL: "https://res.cloudinary.com/dkfch3bjb/image/upload/v1583457226/trash-min_c4nlz8.jpg",
       issue_details: "Needs cleaned up"
     }
   ]);
