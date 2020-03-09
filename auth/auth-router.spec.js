f914000af1823d13b7cceea74729a46e0158c5b0
Juan Aleman <juan.j.aleman@gmail.com>
2020-03-09 17:35:10 -0500

    implemented auth-router tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,76 +1,44 @@
 const request = require("supertest");
 const server = require("../api/server.js");
 
-const db = require("../data/dbConfig.js");
+const db = require("../data/db.config.js");
 
 // Registering user tests
 
 describe("auth-router testing", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+
   describe("registering a user", () => {
-    it("should return 201 ok", () => {
-      return db("users")
-        .truncate()
-        .then(() => {
-          return request(server)
-            .post("/api/auth/register")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(res => {
-          expect(res.status).toBe(201);
-        });
+    it("returns 201 OK", async () => {
+      const user = {
+        email: "aleman@gmail.com",
+        password: "test",
+        full_name: "Aleman test"
+      };
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send(user);
+      expect(res.status).toBe(201);
     });
 
-    it("should return a JSON object", () => {
-      return db("users")
-        .truncate()
-        .then(() => {
-          return request(server)
-            .post("/api/auth/register")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(res => {
-          expect(res.type).toBe("application/json");
-        });
-    });
-  });
-
-  // Logging in tests
-
-  describe("logging in a user", () => {
-    it("should return 200 OK", () => {
-      return db("users")
-        .truncate() 
-        .then(() => {
-          return request(server)
-            .post("/api/auth/register")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(() => {
-          return request(server)
-            .post("/api/auth/login")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
-    });
-
-    it("should return a JSON object", () => {
-      return db("users")
-        .truncate() 
-        .then(() => {
-          return request(server)
-            .post("/api/auth/register")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(() => {
-          return request(server)
-            .post("/api/auth/login")
-            .send({ username: "TestName", password: "test" });
-        })
-        .then(res => {
-          expect(res.type).toBe("application/json");
-        });
+    describe("logging in a user", () => {
+      it("returns 200 OK", async () => {
+        const user = {
+            email: "aleman@gmail.com",
+            password: "test",
+            full_name: "Aleman test"
+          };
+          const userLog = {
+            email: "aleman@gmail.com",
+            password: "test",
+          };
+        await request(server).post('/api/auth/register').send(user)
+        const res = await request(server).post('/api/auth/login').send(userLog)
+        expect(res.status).toBe(200)
+      });
     });
   });
+
 });
