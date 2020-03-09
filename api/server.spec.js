cd9b7ef2f959eb649120610dcc08ab55b898b501
Juan Aleman <juan.j.aleman@gmail.com>
2020-03-09 17:02:10 -0500

    implemented users and server tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const server = require('./server.js');
 const request = require('supertest');
 
 describe('server.js', () => {
     test('should be the testing env', () => {
         expect(process.env.DB_ENV).toBe('testing');
     });
+
+    describe('GET /', function() {
+        it('should return 200 OK', function () {
+            return request(server).get('/')
+            .then( res => {
+                expect(res.status).toBe(200);
+            })
+        })
+
+        it('should return JSON', function () {
+            return request(server).get('/')
+            .then( res => {
+                expect(res.body.message).toBe("It's working!!")
+            })
+        })
+    })
 });
